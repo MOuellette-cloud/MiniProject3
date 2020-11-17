79e2f102590d9f4fb9bfaf080298d83c9244c4cc
Marcel Ouellette <marcel.ouellette@patelhs.org>
2020-11-17 22:19:15 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    int [] seats = {1 , 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10 ,11 , 12 , 13 , 14 , 15};
+//Intiatlize seats variables 1-15
+    String[] students = new String [15];
+//String array of the names
+    for (int i = 15 ; i < students.length; i++) {
+      System.out.println("input name");
+//enter the name
+      names[i] = scan.next();
+
+      System.out.println(students[i]);
+// Create for a loop to ask for the names according to the seat number
+    String Scanner = new Scanner(System.in);
+      String name = name.nextLine();
+      System.out.println("Enter name");
+    }
+  }
+}
